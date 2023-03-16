38fbd8c8bb70886081b841f99205708f8dfe34dc
Ojer Isaac <isaacojerumu@gmail.com>
2023-03-16 22:22:13 +0100

    feat: send emails

---
+++ classes/user.class.php
@@ -0,0 +1,127 @@
+<?php
+
+require_once "db.config.php";
+require_once 'env.class.php';
+$__DotEnvironment = new DotEnvironment(realpath("./.env"));
+
+//reset the timezone default
+date_default_timezone_set('Africa/Lagos');
+
+session_start();
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require '../PHPMailer/src/Exception.php';
+require '../PHPMailer/src/PHPMailer.php';
+require '../PHPMailer/src/SMTP.php';
+
+class User
+{
+
+  private $mail;
+  protected $db;
+
+  /**
+   * @return void
+   */
+  public function __construct()
+  {
+    $this->db = new Database();
+    $this->db = $this->db->connect();
+    $this->mail = new PHPMailer(true);
+    $this->mail->isSMTP();
+    $this->mail->Host = 'lunikdata.com'; 
+    $this->mail->SMTPAuth = true;
+    $this->mail->Username = $_ENV['USERNAME'];
+    $this->mail->Password = $_ENV['PASSWORD'];
+    $this->mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    $this->mail->Port = $_ENV['PORT'];
+  }
+
+  /**
+   * @return string
+   */
+  public function generate_uuid()
+  {
+    $uuid = sprintf(
+      '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+      mt_rand(0, 0xffff),
+      mt_rand(0, 0xffff),
+      mt_rand(0, 0xffff),
+      mt_rand(0, 0x0fff) | 0x4000,
+      mt_rand(0, 0x3fff) | 0x8000,
+      mt_rand(0, 0xffff),
+      mt_rand(0, 0xffff),
+      mt_rand(0, 0xffff)
+    );
+    return $uuid;
+  }
+
+  /**
+   * @param mixed $emails
+   * @param mixed $from_email
+   * @param mixed $name
+   * @param mixed $message
+   * @param mixed $subject
+   * @param mixed $time
+   * @param mixed $code
+   * @return true|void
+   */
+  public function addEmail($emails, $from_email, $name, $message, $subject, $time, $code) {
+    foreach ($emails as $email) {
+      $null = 0;
+      $sql = "INSERT INTO emails (email_address, from_email, name, message, subject, unique_ids, schedule_time, is_job, date)" . "VALUES (?,?,?,?,?,?,?,?,?)";
+      $stmt = $this->db->prepare($sql);
+      $stmt->execute([$email, $from_email, $name, $message, $subject, $code, $time, $null, date('Y-m-d H:i:s')]);
+  
+      return true;
+    }
+  }
+
+  /**
+   * @param mixed $code
+   * @return bool
+   */
+  public function sendEmail($code) {
+    $rows = $this->getEmails($code)->fetch(PDO::FETCH_ASSOC);
+    $emails = explode(",", $rows['email_address']);
+    // print_r($emails); exit;
+
+    // Loop through the emails and send the email to each recipient
+    foreach ($emails as $emailAddress) {
+      try {
+        $this->mail->setFrom($rows['from_email'], $rows['name']);
+        $this->mail->addAddress($emailAddress);
+        $this->mail->isHTML(true);
+        $this->mail->Subject = $rows['subject'];
+        $this->mail->Body = $rows['message'];
+
+        $this->mail->send();
+      } catch (Exception $e) {
+        // Log the error or handle it in some other way
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  /**
+   * @param mixed $code
+   * @return \PDOStatement|false|void
+   */
+  public function getEmails($code)
+  {
+    $sql = "SELECT * from emails WHERE unique_ids = ?";
+    $stmt = $this->db->prepare($sql);
+    $stmt->execute([$code]);
+
+    $count_row = $stmt->rowCount();
+
+    if ($count_row == 1) {
+      return $stmt;
+    }
+  }
+
+}
--- mail.php
+++ mail.php
@@ -1,5 +1,83 @@
+<?php
+
+// Load DotEnvironment Class
+require_once('./classes/env.class.php');
+$__DotEnvironment = new DotEnvironment(realpath("./.env"));
+
+require_once "./classes/user.class.php";
+
+$user = new User();
+
+if (isset($_POST['submit'])) {
+    $required_fields = array('emails', 'senderEmail', 'message', 'senderName', 'subject', 'timeSent');
+    $empty_fields = array();
+
+    foreach ($required_fields as $field) {
+        if (empty($_POST[$field])) {
+            $empty_fields[] = $field;
+        }
+    }
+
+    if (empty($empty_fields)) {
+        switch ($_POST['timeSent']) {
+            case 'now':
+                $time = 1;
+                break;
+            case '5mins':
+                $time = 2;
+                break;
+            case '10mins':
+                $time = 3;
+                break;
+            case '30mins':
+                $time = 4;
+                break;
+            case '1hr':
+                $time = 5;
+                break;
+            default:
+                $time = 1;
+                break;
+        }
+        if ($time == 1) {
+            // generate unique id
+            $code = $user->generate_uuid();
+            // add to queue
+            $email_job = $user->addEmail($_POST['emails'], $_POST['senderEmail'], $_POST['senderName'], $_POST['message'], $_POST['subject'], $time, $code);
+            if ($email_job) {
+                // send mails
+                $send_email = $user->sendEmail($code);
+                if ($send_email) {
+                    $message = "<div style='width: fit-content; margin: 1.2rem auto; color: 43A047;'>Emails sent successfully!</div>";
+
+                    echo "<script type='text/javascript'>";
+                    echo "setTimeout(function() {
+                        window.location.href = 'send-email';";
+                    echo "}, 3500);</script>";
+                } else {
+                    //TODO: Add to queue
+                    $message = '<div style="width: fit-content; margin: 1.2rem auto; color: red;">Something went wrong, emails could not be sent. We would try again in 30secs</div>';
+                }
+            } else {
+                //TODO: Add to queue
+                $message = '<div style="width: fit-content; margin: 1.2rem auto; color: red;">Something went wrong, emails could not be uploaded. We would try again in 30secs</div>';
+            }
+        } else {
+            // generate unique id
+            $code = $user->generate_uuid();
+            // add to queue
+            $email_job = $user->addEmail($_POST['emails'], $_POST['senderEmail'], $_POST['senderName'], $_POST['message'], $_POST['subject'], $time, $code);
+        }
+    } else {
+        $message = '<div style="width: fit-content; margin: 1.2rem auto; color: red;">This fields ' . implode(', ', $empty_fields) . ' cannot be empty</div>';
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,11 +85,12 @@
     <link rel="stylesheet" href="style.css">
     <title>Bulk Email Service</title>
 </head>
+
 <body>
     <div id="loader" style="display: none;">
         <img src="loader.gif" alt="Loading..." />
     </div>
-    <div class="container">  
+    <div class="container">
         <form id="contact" action="" method="post">
             <h3>Bulk email sending service</h3>
             <h4>Schedule your emails</h4>
@@ -34,8 +113,9 @@
             <textarea placeholder="Type your message here...." name="message" tabindex="5" required></textarea>
 
             <label for="timeSent">When you want it sent?</label>
-            <select name="timeSent" id="timeSent">
+            <select name="timeSent" id="timeSent" required>
                 <option value="" selected disabled>--------</option>
+                <option value="now">Now</option>
                 <option value="5mins">5mins later</option>
                 <option value="10mins">10mins later</option>
                 <option value="30mins">30mins later</option>
@@ -44,7 +124,7 @@
 
             <button type="submit" name="submit" style="margin-top: 2rem;">Send Email</button>
 
-            <p class="copyright">Built by <a href="https://wa.me/2348035630576" style="color: #43A047" target="_blank" title="Isaac Ojerumu">Isaac Ojerumu</a></p>
+            <p class="copyright">Built with &#129505; <br> <a href="https://wa.me/2348035630576" style="color: #43A047; text-decoration: none!important;" target="_blank" title="Isaac Ojerumu">Isaac Ojerumu</a></p>
         </form>
     </div>
 
@@ -63,4 +143,5 @@
         });
     </script>
 </body>
+
 </html>
